1d4bdd5207ff39e49ec691ac6a8ba8b86355634e
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2024-05-30 19:29:19 +0900

    Date the fixture data to allow for updated data
    This way existing tests can be kept passing.
    
    Signed-off-by: Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>

--- tests/tcbook_test.rs
+++ tests/tcbook_test.rs
@@ -18,10 +18,11 @@ fn parse_empty_vector() {
 }
 
 #[test]
-fn parse_fixture_tcbook_info() {
+fn parse_fixture_tcbook_info_20240528() {
     let manfest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     // https://kancolle-arcade.net/ac/api/TcBook/info
-    let fixture = path!(Path::new(&manfest_dir) / "tests" / "fixtures" / "TcBook_info.json");
+    let fixture =
+        path!(Path::new(&manfest_dir) / "tests" / "fixtures" / "2024-05-28" / "TcBook_info.json");
 
     let data = fs::read_to_string(fixture).unwrap();
     let tcbook = kca_net::TcBook::new(&data).unwrap();
