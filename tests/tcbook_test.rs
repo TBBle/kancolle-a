b1b1c3cfe49fd512e66f19bd8c21f632dbe52def
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2024-05-30 20:38:51 +0900

    Updated book data: married 扶桑改, gained 大鳳
    Signed-off-by: Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>

--- tests/tcbook_test.rs
+++ tests/tcbook_test.rs
@@ -119,6 +119,20 @@ fn validate_tcbook_common(tcbook: &kca_net::TcBook) {
         assert_ne!(*ship.lv(), 0);
         assert!(ship.is_married().is_some());
         assert!(ship.married_img().is_some());
+        let expected_married_imgs = ship
+            .is_married()
+            .as_ref()
+            .unwrap()
+            .iter()
+            .filter(|is_married| **is_married)
+            .count();
+        assert_eq!(
+            ship.married_img().as_ref().unwrap().len(),
+            expected_married_imgs
+        );
+        // Being married makes the level range 100..=175 rather than 1..=99. However,
+        // we can't validate that here because marrying any renovation
+        // unlocks level 100+ for all renovations, including different book-numbers.
 
         for card_list_page in ship.card_list().iter() {
             for card_image in card_list_page
@@ -148,13 +162,6 @@ fn validate_tcbook_common(tcbook: &kca_net::TcBook) {
                 );
             }
         }
-    }
-
-    // Validate that I have no data, for later updates when I have data.
-    let has_married_img_ships = tcbook.iter().filter(|ship| ship.married_img().is_some());
-    for ship in has_married_img_ships {
-        // No currently-married examples in my data
-        assert_eq!(ship.married_img().as_ref().unwrap().len(), 0);
     }
 }
 
@@ -260,6 +267,128 @@ fn parse_fixture_tcbook_info_20240528() {
     assert_eq!(*扶桑改二.variation_num(), 3);
     assert_eq!(*扶桑改二.acquire_num(), 1);
     assert_eq!(*扶桑改二.lv(), 98);
+    assert_eq!(扶桑改二.is_married().as_ref().unwrap(), &vec![false]);
+    assert_eq!(扶桑改二.married_img().as_ref().unwrap().len(), 0);
+    let 扶桑改二_card_list_0 = &扶桑改二.card_list()[0];
+    assert_eq!(*扶桑改二_card_list_0.priority(), 0);
+    // This is the number of rows on this page. TODO: Rename the field?
+    assert_eq!(扶桑改二_card_list_0.card_img_list().len(), 3);
+    assert_eq!(
+        扶桑改二_card_list_0.card_img_list(),
+        &vec!["s/tc_211_xkrpspyq72qz.jpg", "", "",]
+    );
+    assert_eq!(扶桑改二_card_list_0.status_img().as_ref().unwrap().len(), 1);
+    assert_eq!(
+        扶桑改二_card_list_0.status_img().as_ref().unwrap(),
+        &vec!["i/i_rpyd1nnecq4w_n.png"]
+    );
+    assert_eq!(*扶桑改二_card_list_0.variation_num_in_page(), 3);
+    assert_eq!(*扶桑改二_card_list_0.acquire_num_in_page(), 1);
+}
+
+#[test]
+fn parse_fixture_tcbook_info_20240530() {
+    let manfest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    // https://kancolle-arcade.net/ac/api/TcBook/info
+    let fixture =
+        path!(Path::new(&manfest_dir) / "tests" / "fixtures" / "2024-05-30" / "TcBook_info.json");
+
+    let data = fs::read_to_string(fixture).unwrap();
+    let tcbook = kca_net::TcBook::new(&data).unwrap();
+    assert_eq!(tcbook.len(), 284);
+
+    validate_tcbook_common(&tcbook);
+
+    // Specific interesting ships.
+
+    // I'm sure there's a number of reasons not to name variables like this.
+    let 長門 = &tcbook[0];
+    assert_eq!(*長門.book_no(), 1);
+    assert_eq!(長門.ship_class().as_ref().unwrap(), "長門型");
+    assert_eq!(長門.ship_class_index().unwrap(), 1);
+    assert_eq!(長門.ship_type(), "戦艦");
+    assert_eq!(長門.ship_model_num(), "");
+    assert_eq!(長門.ship_name(), "長門");
+    assert_eq!(長門.card_index_img(), "s/tc_1_d7ju63kolamj.jpg");
+    assert_eq!(長門.card_list().len(), 1);
+    assert_eq!(*長門.variation_num(), 6);
+    assert_eq!(*長門.acquire_num(), 2);
+    assert_eq!(*長門.lv(), 56);
+    assert_eq!(長門.is_married().as_ref().unwrap(), &vec![false, false]);
+    assert_eq!(長門.married_img().as_ref().unwrap().len(), 0);
+    let 長門_card_list_0 = &長門.card_list()[0];
+    assert_eq!(*長門_card_list_0.priority(), 0);
+    // This is the number of rows on this page. TODO: Rename the field?
+    assert_eq!(長門_card_list_0.card_img_list().len(), 6);
+    assert_eq!(
+        長門_card_list_0.card_img_list(),
+        &vec![
+            "s/tc_1_d7ju63kolamj.jpg",
+            "",
+            "",
+            "s/tc_1_2wp6daq4fn42.jpg",
+            "",
+            ""
+        ]
+    );
+    assert_eq!(長門_card_list_0.status_img().as_ref().unwrap().len(), 2);
+    assert_eq!(
+        長門_card_list_0.status_img().as_ref().unwrap(),
+        &vec!["i/i_d7ju63kolamj_n.png", "i/i_2wp6daq4fn42_n.png"]
+    );
+    assert_eq!(*長門_card_list_0.variation_num_in_page(), 6);
+    assert_eq!(*長門_card_list_0.acquire_num_in_page(), 2);
+
+    // Interesting because I hit level 99 and triggered 扶桑改's ケッコンカッコカリ
+    let 扶桑 = &tcbook[25];
+    assert_eq!(*扶桑.book_no(), 26);
+    assert_eq!(扶桑.ship_class().as_ref().unwrap(), "扶桑型");
+    assert_eq!(扶桑.ship_class_index().unwrap(), 1);
+    assert_eq!(扶桑.ship_type(), "戦艦");
+    assert_eq!(扶桑.ship_model_num(), "");
+    assert_eq!(扶桑.ship_name(), "扶桑");
+    assert_eq!(扶桑.card_index_img(), "s/tc_26_p9u490qtc1a4.jpg");
+    assert_eq!(扶桑.card_list().len(), 1);
+    assert_eq!(*扶桑.variation_num(), 6);
+    assert_eq!(*扶桑.acquire_num(), 4);
+    assert_eq!(*扶桑.lv(), 100);
+    assert_eq!(扶桑.is_married().as_ref().unwrap(), &vec![false, true]);
+    assert_eq!(扶桑.married_img().as_ref().unwrap().len(), 1);
+    let 扶桑_card_list_0 = &扶桑.card_list()[0];
+    assert_eq!(*扶桑_card_list_0.priority(), 0);
+    // This is the number of rows on this page. TODO: Rename the field?
+    assert_eq!(扶桑_card_list_0.card_img_list().len(), 6);
+    assert_eq!(
+        扶桑_card_list_0.card_img_list(),
+        &vec![
+            "s/tc_26_p9u490qtc1a4.jpg",
+            "",
+            "",
+            "s/tc_26_fskeangzj9cz.jpg",
+            "s/tc_26_z2jfdzutu1j3.jpg",
+            "s/tc_26_krjdrps6k23r.jpg"
+        ]
+    );
+    assert_eq!(扶桑_card_list_0.status_img().as_ref().unwrap().len(), 2);
+    assert_eq!(
+        扶桑_card_list_0.status_img().as_ref().unwrap(),
+        &vec!["i/i_p9u490qtc1a4_n.png", "i/i_fskeangzj9cz_n.png"]
+    );
+    assert_eq!(*扶桑_card_list_0.variation_num_in_page(), 6);
+    assert_eq!(*扶桑_card_list_0.acquire_num_in_page(), 4);
+
+    let 扶桑改二 = &tcbook[200];
+    assert_eq!(*扶桑改二.book_no(), 211);
+    assert_eq!(扶桑改二.ship_class().as_ref().unwrap(), "扶桑型");
+    assert_eq!(扶桑改二.ship_class_index().unwrap(), 1);
+    assert_eq!(扶桑改二.ship_type(), "航空戦艦");
+    assert_eq!(扶桑改二.ship_model_num(), "");
+    assert_eq!(扶桑改二.ship_name(), "扶桑改二");
+    assert_eq!(扶桑改二.card_index_img(), "s/tc_211_xkrpspyq72qz.jpg");
+    assert_eq!(扶桑改二.card_list().len(), 1);
+    assert_eq!(*扶桑改二.variation_num(), 3);
+    assert_eq!(*扶桑改二.acquire_num(), 1);
+    assert_eq!(*扶桑改二.lv(), 100);
     assert_eq!(扶桑改二.is_married().as_ref().unwrap(), &vec![false]);
     assert_eq!(扶桑改二.married_img().as_ref().unwrap().len(), 0);
     let 扶桑改二_card_list_0 = &扶桑改二.card_list()[0];
